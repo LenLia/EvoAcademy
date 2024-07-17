3feaff8f73bb447444623983ca4dd9d783e8d0da
Lena <samylkinal@ya.ru>
2024-07-17 23:18:07 +0300

    Tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FirstEvoApp';
+export class AppComponent implements OnInit {
+  title1 = 'FirstEvoApp';
+
+  constructor(private meta: Meta){
+    this.meta.addTag({name: 'description', content: 'root_desc'});
+  }
+
+  ngOnInit(): void {
+
+  }
 }
