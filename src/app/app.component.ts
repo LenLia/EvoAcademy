06d24de37e687e977d711501e93f5de33fee0382
Lena <samylkinal@ya.ru>
2024-07-20 19:47:55 +0300

    Form Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { audit, first } from 'rxjs';
+import { Book } from './book';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'FirstEvoApp';
+
+  books: Book[]=[
+    {
+
+      name: 'CaptainsDaughter',
+      author:'Pushkin'
+    },
+    {
+
+      name: 'DogsHeart',
+      author:'Bulgakov'
+    },
+    {
+
+      name: 'Mtsyri',
+      author:'Lermontov'
+    }
+  ];
+
+  create_book={
+    name: null,
+    author: null
+  }
+
+  createBook(){
+    this.books.push({
+      name: this.create_book.name,
+      author: this.create_book.author,
+  })
+  }
+
+  
 }
---
+++ src/app/book.ts
@@ -0,0 +1,5 @@
+export interface Book {
+
+    name: string | null;
+    author: string | null;
+}
