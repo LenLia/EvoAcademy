1df2fa6f666b9c8422fcc53900a4a22268c005a0
Lena <samylkinal@ya.ru>
2024-07-22 18:21:23 +0300

    RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { filter, interval, map, Observable, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstEvoApp';
+
+  randomSubscrip$!: Subscription;
+  sequentiallySubscrip$!: Subscription;
+  
+  randomArray: string[]=[];
+  sequentiallyArray: number[]=[];
+
+  enableSequentiallyStopButton = false;
+  enableRandomStopButton = false;
+
+
+  enableSequentiallyFunction(){
+    const interval$ = interval(2000);
+    this.sequentiallySubscrip$ = interval$.subscribe(
+      (value) =>{
+        this.sequentiallyArray.push(value);
+      })
+
+    this.enableSequentiallyStopButton=true;
+  }
+
+  disableSequentiallyFunction(){
+    this.sequentiallySubscrip$.unsubscribe();
+    this.enableSequentiallyStopButton=false;
+  }
+
+
+  enableRandomFunction(){
+    const interval$ = interval(2000);
+    this.randomSubscrip$ = interval$.pipe(
+      map(() => `Rundom Value:  ${Math.floor(Math.random()*1000)}`)
+    ).subscribe((value) =>{
+      this.randomArray.push(value);
+    })
+
+    this.enableRandomStopButton=true;
+
+  }
+
+  disableRandomFunction(){
+    this.randomSubscrip$.unsubscribe();
+    this.enableRandomStopButton=false;
+
+  }
+
+
+  
 }
