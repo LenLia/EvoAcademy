29dae79a44a6d8f9a9306f5d93668e7adc30bb4d
Lena <samylkinal@ya.ru>
2024-07-27 17:34:05 +0300

    HttpInterceptors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'FirstEvoApp';
+  constructor(private dataService: DataService){}
+
+  todos: object[]=[]
+
+  ngOnInit(): void {
+      this.dataService.getTodos().subscribe({
+        next: (resp: any) =>{
+          this.todos=resp;
+          console.log(resp)
+        },
+        error: (err: HttpErrorResponse) =>{console.log(err.error)}
+      })
+  }
+
 }
---
+++ src/app/logging.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders,
+  HttpResponse
+} from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log(request)
+    request = request.clone({url: request.url+'/1'})
+    return next.handle(request)
+  }
+}
